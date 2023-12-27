8d3ce749087a00106b20a07758c3a27b20a34d23
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-12-27 18:18:14 +0000

    tweak: do not add extra header

--- src/gc.zig
+++ src/gc.zig
@@ -86,8 +86,9 @@ pub const GcAllocator = struct {
         return_address: usize,
     ) ?[*]u8 {
         _ = return_address;
-        assert(len > 0);
-        return alignedAlloc(len, log2_align);
+        const alignment = @as(usize, 1) << @as(Allocator.Log2Align, @intCast(log2_align));
+        const ptr = c.GC_memalign(alignment, len + alignment - 1 + @sizeOf(usize));
+        return @ptrCast(ptr);
     }
 
     fn resize(
@@ -99,16 +100,8 @@ pub const GcAllocator = struct {
     ) bool {
         _ = log2_buf_align;
         _ = return_address;
-        if (new_len <= buf.len) {
-            return true;
-        }
-
-        const full_len = alignedAllocSize(buf.ptr);
-        if (new_len <= full_len) {
-            return true;
-        }
-
-        return false;
+        const full_len = c.GC_size(buf.ptr);
+        return new_len <= full_len;
     }
 
     fn free(
@@ -119,37 +112,7 @@ pub const GcAllocator = struct {
     ) void {
         _ = log2_buf_align;
         _ = return_address;
-        alignedFree(buf.ptr);
-    }
-
-    fn getHeader(ptr: [*]u8) *[*]u8 {
-        return @as(*[*]u8, @ptrFromInt(@intFromPtr(ptr) - @sizeOf(usize)));
-    }
-
-    fn alignedAlloc(len: usize, log2_align: u8) ?[*]u8 {
-        const alignment = @as(usize, 1) << @as(Allocator.Log2Align, @intCast(log2_align));
-
-        // Thin wrapper around regular malloc, overallocate to account for
-        // alignment padding and store the orignal malloc()'ed pointer before
-        // the aligned address.
-        const unaligned_ptr = @as([*]u8, @ptrCast(c.GC_malloc(len + alignment - 1 + @sizeOf(usize)) orelse return null));
-        const unaligned_addr = @intFromPtr(unaligned_ptr);
-        const aligned_addr = mem.alignForward(usize, unaligned_addr + @sizeOf(usize), alignment);
-        const aligned_ptr = unaligned_ptr + (aligned_addr - unaligned_addr);
-        getHeader(aligned_ptr).* = unaligned_ptr;
-
-        return aligned_ptr;
-    }
-
-    fn alignedFree(ptr: [*]u8) void {
-        const unaligned_ptr = getHeader(ptr).*;
-        c.GC_free(unaligned_ptr);
-    }
-
-    fn alignedAllocSize(ptr: [*]u8) usize {
-        const unaligned_ptr = getHeader(ptr).*;
-        const delta = @intFromPtr(ptr) - @intFromPtr(unaligned_ptr);
-        return c.GC_size(unaligned_ptr) - delta;
+        c.GC_free(buf.ptr);
     }
 };
 
